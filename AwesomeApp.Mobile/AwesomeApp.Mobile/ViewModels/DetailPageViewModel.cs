a4309de781545606db131a99bb615e2ad37dace5
Beda Bogdan <bogdan.beda@force24.co.uk>
2019-11-22 10:41:29 +0200

    Add project files.

---
+++ AwesomeApp.Mobile/AwesomeApp.Mobile/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AwesomeApp.Mobile.ViewModels
+{
+    public class DetailPageViewModel : INotifyPropertyChanged
+    {
+        private string noteText;
+
+        public DetailPageViewModel(string note)
+        {
+            NoteText = note;
+            DismissPageCommand = new Command(async () => await Application.Current.MainPage.Navigation.PopModalAsync());
+
+        }
+
+        public string NoteText
+        {
+            get
+            {
+                return noteText;
+            }
+            set
+            {
+                noteText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NoteText)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command DismissPageCommand { get; }
+    }
+}
---
+++ AwesomeApp.Mobile/AwesomeApp.Mobile/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AwesomeApp.Mobile.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+
+        public MainPageViewModel()
+        {
+            EraseCommand = new Command(() => TheNote = string.Empty);
+            SaveCommand = new Command(() => { AllNotes.Add(TheNote); TheNote = string.Empty; });
+            SelectedNoteChangedCommand = new Command(async () =>
+            {
+                var detailVM = new DetailPageViewModel(SelectedNote);
+                var detailPage = new DetailPage();
+                detailPage.BindingContext = detailVM;
+                await Application.Current.MainPage.Navigation.PushModalAsync(detailPage);
+            });
+        }
+
+        public ObservableCollection<string> AllNotes { get; } = new ObservableCollection<string>();
+
+        private string theNote;
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TheNote)));
+            }
+        }
+
+        private string selectedNote;
+        public string SelectedNote
+        {
+            get => selectedNote;
+            set
+            {
+                selectedNote = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedNote)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command SaveCommand { get; }
+        public Command EraseCommand { get; }
+        public Command SelectedNoteChangedCommand { get; }
+
+
+
+    }
+}
